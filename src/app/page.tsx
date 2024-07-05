9b2a499a14d45b5a5affc6beb1d52cb7ef743e19
Loussararian Antony <antony.loussararian@gmail.com>
2024-07-05 14:40:00 +0200

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,8 @@
 "use client";
-
-import { type BaseError, useWriteContract, useWaitForTransactionReceipt } from 'wagmi';
-import {abi} from '../../hardhat/artifacts/contracts/NFTFactory.sol/NFTFactory.json'; 
+import { MintNFT } from '@/components/MintNFT';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 
-const NFT_FACTORY_ADDRESS = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
-
 function Page() {
-  const { 
-    data: hash, 
-    isPending, 
-    error,
-    writeContract 
-  } = useWriteContract()
-
-  const { isLoading: isConfirming, isSuccess: isConfirmed } =
-    useWaitForTransactionReceipt({
-      hash,
-  })
-
-  async function mintNFT(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault()
-    const formData = new FormData(e.target as HTMLFormElement)
-    const name = formData.get('name') as string
-    const symbol = formData.get('symbol') as string
-    writeContract({
-      address: NFT_FACTORY_ADDRESS,
-      abi,
-      functionName: 'generateNFT',
-      args: [String(name), String(symbol)],
-    })
-  }
 
   return (
     <div>
@@ -44,22 +16,7 @@ function Page() {
         <ConnectButton showBalance={true}/>
       </div>
       <div style={{ padding: 20 }}>
-        <form onSubmit={mintNFT}>
-          <input name="name" placeholder="MyNFT" required />
-          <input name="symbol" placeholder="MNFT" required />
-          <button 
-          disabled={isPending} 
-          type="submit"
-          >
-            {isPending ? 'Confirming...' : 'Mint'}
-          </button>
-          {hash && <div>Transaction Hash: {hash}</div>}
-          {isConfirming && <div>Waiting for confirmation...</div>}
-          {isConfirmed && <div>Transaction confirmed.</div>}
-          {error && (
-            <div>Error: {(error as BaseError).shortMessage || error.message}</div>
-          )}
-        </form>
+        <MintNFT />
       </div>  
     </div>
   );
---
+++ src/components/MintNFT.tsx
@@ -0,0 +1,56 @@
+import * as React from 'react';
+import { 
+  type BaseError,
+  useWaitForTransactionReceipt, 
+  useWriteContract 
+} from 'wagmi';
+import {abi} from '../../hardhat/artifacts/contracts/NFTFactory.sol/NFTFactory.json'; 
+
+const NFT_FACTORY_ADDRESS = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+
+export function MintNFT() {
+    const {
+        data: hash,
+        isPending,
+        error,
+        writeContract
+    } = useWriteContract()
+
+    const { isLoading: isConfirming, isSuccess: isConfirmed } =
+        useWaitForTransactionReceipt({
+            hash,
+        })
+
+    async function submit(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault()
+        const formData = new FormData(e.target as HTMLFormElement)
+        const name = formData.get('name') as string
+        const symbol = formData.get('symbol') as string
+        writeContract({
+            address: NFT_FACTORY_ADDRESS,
+            abi,
+            functionName: 'generateNFT',
+            args: [String(name), String(symbol)],
+        })
+    }
+
+    return (
+        <form onSubmit={submit}>
+          <input name="name" placeholder="MyNFT" required />
+          <input name="symbol" placeholder="MNFT" required />
+          <button 
+          disabled={isPending} 
+          type="submit"
+          >
+            {isPending ? 'Confirming...' : 'Mint'}
+          </button>
+          {hash && <div>Transaction Hash: {hash}</div>}
+          {isConfirming && <div>Waiting for confirmation...</div>}
+          {isConfirmed && <div>Transaction confirmed.</div>}
+          {error && (
+            <div>Error: {(error as BaseError).shortMessage || error.message}</div>
+          )}
+        </form>
+    )
+
+}
