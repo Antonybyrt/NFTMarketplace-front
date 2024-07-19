3f01bfae4ea866b4c44e6a73ad0d635e361f3ea0
Bentalha Aymen <>
2024-07-19 20:05:29 +0200

    image manquante

--- src/components/list/NftGrid/NftGrid.tsx
+++ src/components/list/NftGrid/NftGrid.tsx
@@ -8,11 +8,12 @@ interface NftGridProps {
 }
 
 const NftGrid: React.FC<NftGridProps> = ({ nfts }) => {
+  const defaultImage = '/image/missing_donut.png';
   return (
     <div className="nft-grid">
       {nfts.map((nft) => (
         <div key={nft.id} className="nft-item">
-          <img className="nft-image" src={nft.image} alt={nft.name} />
+          <img className="nft-image" src={nft.image || defaultImage} alt={nft.name} />
           <div className="nft-details">
             <h2 className="nft-title">{nft.name}</h2>
             {nft.description && <p className="nft-description">{nft.description}</p>}
