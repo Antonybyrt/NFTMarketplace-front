1e8932da3f22ed35bf9cd312d8ff8847ed32cd66
Bentalha Aymen <>
2024-07-22 15:56:19 +0200

    upload.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import { CreateCollection } from '@/components/CreateCollection';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import UploadImage from '@/components/upload/upload';
 
 function Page() {
   return (
@@ -9,7 +10,7 @@ function Page() {
       <div className="background-container">
         <div className="overlay"></div>
         <div className="content">
-          
+          <UploadImage/>
         </div>
       </div>
     </div>
